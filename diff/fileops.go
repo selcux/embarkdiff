de39d13348dc184e27ff2f34b0d95ecb9c4a2782
Selçuk Öztürk <selcukahmed@gmail.com>
2021-02-02 11:45:31 +0300

    [UPDATE] fileops.go comments

--- diff/fileops.go
+++ diff/fileops.go
@@ -7,11 +7,13 @@ import (
 	"strings"
 )
 
+//FileOperation defines the operation type for the related file
 type FileOperation struct {
 	Operation SyncOperation
 	FileData
 }
 
+//PathDepth returns the hierarchical length of the file or directory
 func (f FileOperation) PathDepth() int {
 	dir := f.Path
 	if !f.IsDir {
@@ -27,12 +29,14 @@ func (f FileOperation) PathDepth() int {
 
 type FileOps []FileOperation
 
+//SortByPathLen sorts the files according to the increasing hierarchical depth
 func (fo FileOps) SortByPathLen() {
 	sort.Slice(fo, func(i, j int) bool {
 		return fo[i].PathDepth() < fo[j].PathDepth()
 	})
 }
 
+//GroupByOps groups the files by the operation type
 func (fo FileOps) GroupByOps() map[SyncOperation]FileOps {
 	groupMap := make(map[SyncOperation]FileOps)
 
@@ -47,17 +51,27 @@ func (fo FileOps) GroupByOps() map[SyncOperation]FileOps {
 	return groupMap
 }
 
+//Reverse the order of files in the slice
 func (fo FileOps) Reverse() {
 	for i, j := 0, len(fo)-1; i < j; i, j = i+1, j-1 {
 		fo[i], fo[j] = fo[j], fo[i]
 	}
 }
 
+/*
+Transform the file array into required operation order.
+The required order is defined by the order of `SyncOperation`
+
+1. Delete the files and folders in a bottom up hierarchy
+2. Create the missing folders
+3. Copy the changed or the missing files
+*/
 func (fo FileOps) Transform() FileOps {
-	return convert(fo.GroupByOps())
+	return flatten(fo.GroupByOps())
 }
 
-func convert(groupMap map[SyncOperation]FileOps) FileOps {
+//flatten the grouped elements into an array
+func flatten(groupMap map[SyncOperation]FileOps) FileOps {
 	fops := make(FileOps, 0)
 
 	for i := SyncOperation(0); i <= Copy; i++ {
